97bb9577533bc37fc93b5f2ec9f1a5e71381a911
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2022-10-02 09:03:54 +0300

    Dilog Wibdow in JS

---
+++ BLOCK JS/Urok4.5.js
@@ -0,0 +1,18 @@
+// создаеи объект
+let obj = [
+{
+    "color": "Tomato",
+    "numbers": [`8888`, `7777`],
+    "checked": true
+},
+{
+    "color": "Green",
+    "numbers": [`66`, `11`],
+    "checked": false
+},
+{
+    "color": "TBlue",
+    "numbers": [`7751`, `3247`],
+    "checked": true
+}
+]
---
+++ Practica/PRK4.5.js
@@ -0,0 +1,6 @@
+let obj = {
+    "name": "Bella", 
+    "hight": [`450`,`800`],
+    "color": [`black`,`white`],
+    "paws": `40`,
+}
