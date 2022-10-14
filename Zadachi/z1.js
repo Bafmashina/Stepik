35afd3222ef5077a476f3db5252b27e40b992820
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2022-10-14 09:20:45 +0300

    Сортировка

--- Zadachi/z1.js
+++ Zadachi/z1.js
@@ -44,22 +44,57 @@
 // ===========================================
 // Задача 7 
 // Найти колтчество положительных
-let positive = 0; negative = 0
-let a = 5, b = -6, c = -1
+// let positive = 0; negative = 0
+// let a = 5, b = -6, c = -1
 
-if(a > 0) {
-    positive++
-} else if(a < 0) {
-    negative++
+// if(a > 0) {
+//     positive++
+// } else if(a < 0) {
+//     negative++
+// }
+// if(b > 0) {
+//     positive++
+// } else if(b < 0) {
+//     negative++
+// }
+// if(c > 0) {
+//     positive++
+// } else if(c < 0) {
+//     negative++
+// }
+// console.log(`Положительных: ${positive} \n Отрицательных: ${negative}`)
+// ============================================================================
+// Задача 8
+// let day =  1
+// switch(day) {
+//     case 1 :
+//         alert(`Понедельник`);
+//         break
+//     case 2 :
+//         alert(`Вторник`);
+//         break
+//     case 3 :
+//         alert(`Среда`);
+//         break
+//     case 4 :
+//         alert(`Четверг`);
+//         break
+//     case 5 :
+//         alert(`Пятница`);
+//         break
+//     case 6 :
+//         alert(`Суббота`);
+//         break
+//     case 7 :
+//         alert(`Воскресенье`);
+//         break
+//         default:
+//             alert(`Неизвестно`)
+// }
+// =============================================================
+// Задача 9
+// Вывести N раз число K
+let k = 4, n = 8
+for(let i =0; i<n; i++) {
+    console.log(k)
 }
-if(b > 0) {
-    positive++
-} else if(b < 0) {
-    negative++
-}
-if(c > 0) {
-    positive++
-} else if(c < 0) {
-    negative++
-}
-console.log(`Положительных: ${positive} \n Отрицательных: ${negative}`)
